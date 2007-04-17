228cb60d176f2533a388e66c5e84751b96bc8dfd
Andrew Chilton <andychilton@gmail.com>
2007-04-17 10:55:28 +0000

    - main bulk of the updated Blog

--- lib/Zaapt/Store/Pg/Blog.pm
+++ lib/Zaapt/Store/Pg/Blog.pm
@@ -4,6 +4,7 @@ use base qw( Zaapt::Store::Pg Zaapt::Model::Blog );
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 use Zaapt::Store::Pg::Account;
 use Zaapt::Store::Pg::Common;
@@ -17,7 +18,7 @@ my $table = {
     blog => {
         name => 'blog',
         prefix => 'b',
-        cols => [ qw(id name title description show comment trackback r:admin_id r:view_id r:edit_id r:publish_id ts:inserted ts:updated) ],
+        cols => [ qw(id name title description show moderate comment trackback r:admin_id r:view_id r:edit_id r:publish_id ts:inserted ts:updated) ],
     },
     entry => {
         name => 'entry',
@@ -27,7 +28,7 @@ my $table = {
             [ 'blog_id', 'fk', 'b_id' ],
             [ 'account_id', 'fk', 'a_id' ],
             [ 'type_id', 'fk', 't_id' ],
-            qw(name title intro article draft comment trackback ts:inserted ts:updated)
+            qw(name title intro article draft comment trackback ro:comments ro:trackbacks ts:inserted ts:updated)
         ],
     },
     entry_label => {
@@ -66,6 +67,7 @@ my $join = {
     e_l  => "JOIN $schema.entry_label el ON (el.entry_id = e.id) JOIN common.label l ON (el.label_id = l.id)",
     e_c  => "JOIN $schema.comment c ON (c.entry_id = e.id)",
     e_tr => "JOIN $schema.trackback tr ON (tr.entry_id = e.id)",
+    e_a  => "JOIN account.account a ON (e.account_id = a.id)",
 };
 
 ## ----------------------------------------------------------------------------
@@ -83,7 +85,12 @@ __PACKAGE__->_mk_sql_for( $table->{account}{schema}, $table->{account} );
 $table->{type} = Zaapt::Store::Pg::Common->get_table_details('type');
 __PACKAGE__->_mk_sql_for( $table->{type}{schema}, $table->{type} );
 
+$table->{label} = Zaapt::Store::Pg::Common->get_table_details('label');
+__PACKAGE__->_mk_sql_for( $table->{label}{schema}, $table->{label} );
+
 ## ----------------------------------------------------------------------------
+
+# print Data::Dumper->Dump([$table], ['table']);
 
 # blog
 __PACKAGE__->mk_selecter( $schema, $table->{blog}{name}, $table->{blog}{prefix}, @{$table->{blog}{cols}} );
@@ -91,8 +98,8 @@ __PACKAGE__->mk_selecter_using( $schema, $table->{blog}{name}, $table->{blog}{pr
 __PACKAGE__->mk_select_rows( 'sel_blog_all', "SELECT $table->{blog}{sql_sel_cols} FROM $table->{blog}{sql_fqt} ORDER BY b.id", [] );
 
 # entry
-my $blog_cols = "$table->{blog}{sql_sel_cols}, $table->{entry}{sql_sel_cols}, $table->{type}{sql_sel_cols}";
-my $blog_joins = "$table->{blog}{sql_fqt} $join->{b_e} $join->{e_t}";
+my $blog_cols = "$table->{blog}{sql_sel_cols}, $table->{entry}{sql_sel_cols}, $table->{type}{sql_sel_cols}, $table->{account}{sql_sel_cols}";
+my $blog_joins = "$table->{blog}{sql_fqt} $join->{b_e} $join->{e_t} $join->{e_a}";
 
 __PACKAGE__->mk_select_row( 'sel_entry', "SELECT $blog_cols FROM $blog_joins WHERE e.id = ?", [ 'e_id' ] );
 
@@ -100,12 +107,20 @@ __PACKAGE__->mk_select_row( 'sel_entry_in_blog', "SELECT $blog_cols FROM $blog_j
 
 __PACKAGE__->mk_select_rows( 'sel_entry_all_in', "SELECT $blog_cols FROM $blog_joins WHERE b.id = ? ORDER BY e.inserted DESC", [ 'b_id' ] );
 
-__PACKAGE__->mk_select_rows( 'sel_entry_latest_in', "SELECT $blog_cols FROM $blog_joins WHERE b.id = ? ORDER BY e.inserted DESC LIMIT ?", [ 'b_id', '_limit' ] );
+__PACKAGE__->mk_select_rows( 'sel_entry_latest', "SELECT $blog_cols FROM $blog_joins WHERE b.id = ? ORDER BY e.inserted DESC LIMIT ?", [ 'b_id', '_limit' ] );
 
-__PACKAGE__->mk_select_rows( 'sel_entry_archive_in', "SELECT $blog_cols FROM $blog_joins WHERE b.id = ? AND e.inserted >= ?::DATE AND e.inserted <= ?::DATE ORDER BY e.inserted DESC", [ 'b_id', '_from', '_to' ] );
+__PACKAGE__->mk_select_rows( 'sel_entry_archive', "SELECT $blog_cols FROM $blog_joins WHERE b.id = ? AND e.inserted >= ?::DATE AND e.inserted <= ?::DATE + ?::INTERVAL ORDER BY e.inserted DESC", [ 'b_id', '_from', '_from', '_for' ] );
+
+__PACKAGE__->mk_select_rows( 'sel_label_all_for', "SELECT $table->{label}{sql_sel_cols} FROM $table->{entry}{sql_fqt} $join->{e_l} WHERE e.id = ? ORDER BY l.name", [ 'e_id' ] );
 
 # entry_label
 my $ins_entry_label = __PACKAGE__->_mk_ins( 'blog.entry_label', qw(entry_id label_id) );
+
+# comment
+__PACKAGE__->mk_select_rows( 'sel_comments_for', "SELECT $table->{comment}{sql_sel_cols} FROM $table->{entry}{sql_fqt} $join->{e_c} WHERE e.id = ? AND c.status = ? ORDER BY c.inserted", [ 'e_id', 'c_status' ] );
+
+# trackbacks
+__PACKAGE__->mk_select_rows( 'sel_trackbacks_for', "SELECT $table->{trackback}{sql_sel_cols} FROM $table->{entry}{sql_fqt} $join->{e_tr} WHERE e.id = ? AND tr.status = ? ORDER BY tr.inserted", [ 'e_id', 'tr_status' ] );
 
 ## ----------------------------------------------------------------------------
 # methods
@@ -115,12 +130,13 @@ sub ins_label {
 
     my $common_model = $self->parent()->get_model('Common');
 
-    warn "c_m=" . ref $common_model;
-
-    $self->dbh()->begin_work();
     my $label = $common_model->ass_label( $hr );
     $self->_do( $ins_entry_label, $hr->{e_id}, $label->{l_id} );
-    $self->dbh()->commit();
+}
+
+sub del_entry_label_for {
+    my ($self, $hr) = @_;
+    $self->_do("DELETE FROM blog.entry_label WHERE entry_id = ?", $hr->{e_id});
 }
 
 sub _nuke {
--- store/Pg/create_blog.sql
+++ store/Pg/create_blog.sql
@@ -10,14 +10,15 @@ CREATE TABLE blog.blog (
     title           TEXT NOT NULL,
     description     TEXT NOT NULL,
     show            INTEGER NOT NULL,
+    moderate        BOOLEAN NOT NULL DEFAULT False,
+    comment         BOOLEAN NOT NULL DEFAULT False,
+    trackback       BOOLEAN NOT NULL DEFAULT False,
     admin_id        INTEGER NOT NULL REFERENCES account.role,
     view_id         INTEGER NOT NULL REFERENCES account.role,
     edit_id         INTEGER NOT NULL REFERENCES account.role,
     publish_id      INTEGER NOT NULL REFERENCES account.role,
 --    comment_id      INTEGER NOT NULL REFERENCES account.role,
 --    trackback_id    INTEGER NOT NULL REFERENCES account.role,
-    comment         BOOLEAN NOT NULL DEFAULT False,
-    trackback       BOOLEAN NOT NULL DEFAULT False,
 
     UNIQUE(name),
     LIKE base       INCLUDING DEFAULTS
@@ -30,7 +31,7 @@ COMMENT ON COLUMN blog.blog.admin_id IS
 COMMENT ON COLUMN blog.blog.view_id IS
     'View allows the user to see the blog and it\'s entries';
 COMMENT ON COLUMN blog.blog.edit_id IS
-    'Edit allows the user manipulate the entries (add/edit/del/tags)';
+    'Edit allows the user manipulate the entries (add/edit/del/labels)';
 COMMENT ON COLUMN blog.blog.publish_id IS
     'Publish allows the user to publish or revoke any entries';
 -- COMMENT ON COLUMN blog.blog.comment_id IS
@@ -43,6 +44,7 @@ CREATE SEQUENCE blog.entry_id_seq;
 CREATE TABLE blog.entry (
     id              INTEGER NOT NULL DEFAULT nextval('blog.entry_id_seq'::TEXT) PRIMARY KEY,
     blog_id         INTEGER NOT NULL REFERENCES blog.blog,
+    account_id      INTEGER NOT NULL REFERENCES account.account,
     type_id         INTEGER NOT NULL REFERENCES common.type,
     name            TEXT NOT NULL,
     title           TEXT NOT NULL,
@@ -51,6 +53,8 @@ CREATE TABLE blog.entry (
     draft           BOOLEAN DEFAULT False,
     comment         BOOLEAN NOT NULL DEFAULT False,
     trackback       BOOLEAN NOT NULL DEFAULT False,
+    comments        INT NOT NULL DEFAULT 0,
+    trackbacks      INT NOT NULL DEFAULT 0,
 
     UNIQUE(blog_id, name),
     LIKE base       INCLUDING DEFAULTS
@@ -60,14 +64,16 @@ CREATE TRIGGER entry_updated BEFORE UPDATE ON blog.entry
 CREATE INDEX entry_inserted ON blog.entry(inserted);
 
 -- table: entry_label
+CREATE SEQUENCE blog.entry_label_id_seq;
 CREATE TABLE blog.entry_label (
+    id              INTEGER NOT NULL DEFAULT nextval('blog.entry_label_id_seq'::TEXT) PRIMARY KEY,
     entry_id        INTEGER NOT NULL REFERENCES blog.entry,
     label_id        INTEGER NOT NULL REFERENCES common.label,
 
     UNIQUE(entry_id, label_id),
     LIKE base       INCLUDING DEFAULTS
 );
-CREATE TRIGGER tag_updated BEFORE UPDATE ON blog.entry_label
+CREATE TRIGGER entry_label_updated BEFORE UPDATE ON blog.entry_label
     FOR EACH ROW EXECUTE PROCEDURE updated();
 
 -- table: comment
@@ -101,5 +107,109 @@ CREATE TABLE blog.trackback (
 );
 CREATE TRIGGER trackback_updated BEFORE UPDATE ON blog.trackback
     FOR EACH ROW EXECUTE PROCEDURE updated();
+
+-- functions
+
+-- function: comment_bi
+CREATE FUNCTION blog.comment_bi() RETURNS trigger as '
+    BEGIN
+        IF NEW.status = ''acc'' THEN
+            UPDATE blog.entry SET comments = comments + 1 WHERE id = NEW.entry_id;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER comment_ai BEFORE INSERT ON blog.comment
+    FOR EACH ROW EXECUTE PROCEDURE blog.comment_bi();
+
+-- function: comment_bu
+-- Note: doing this BU since it can then fail, but if we did it AU would it be
+-- out of sync? No, I thought not, but if someone knows the correct answer, or
+-- indeed whether this even makes a difference, please let me know.
+CREATE OR REPLACE FUNCTION blog.comment_bu() RETURNS trigger as '
+    BEGIN
+        IF OLD.entry_id = NEW.entry_id THEN
+            IF OLD.status <> ''acc'' AND NEW.status = ''acc'' THEN
+                UPDATE blog.entry SET comments = comments + 1 WHERE id = OLD.entry_id;
+            END IF;
+            IF OLD.status = ''acc'' AND NEW.status <> ''acc'' THEN
+                UPDATE blog.entry SET comments = comments - 1 WHERE id = OLD.entry_id;
+            END IF;
+        ELSE
+            IF OLD.status = ''acc'' THEN
+                UPDATE blog.entry SET comments = comments - 1 WHERE id = OLD.entry_id;
+            END IF;
+            IF NEW.status = ''acc'' THEN
+                UPDATE blog.entry SET comments = comments + 1 WHERE id = NEW.entry_id;
+            END IF;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER comment_bu BEFORE UPDATE ON blog.comment
+    FOR EACH ROW EXECUTE PROCEDURE blog.comment_bu();
+
+-- function: comment_bd
+CREATE FUNCTION blog.comment_bd() RETURNS trigger as '
+    BEGIN
+        IF OLD.status = ''acc'' THEN
+            UPDATE blog.entry SET comments = comments - 1 WHERE id = OLD.entry_id;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER comment_bd BEFORE DELETE ON blog.comment
+    FOR EACH ROW EXECUTE PROCEDURE blog.comment_bd();
+
+-- function: trackback_bi
+CREATE FUNCTION blog.trackback_bi() RETURNS trigger as '
+    BEGIN
+        IF NEW.status = ''acc'' THEN
+            UPDATE blog.entry SET trackbacks = trackbacks + 1 WHERE id = NEW.entry_id;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER trackback_ai BEFORE INSERT ON blog.trackback
+    FOR EACH ROW EXECUTE PROCEDURE blog.trackback_bi();
+
+-- function: trackback_bu
+-- Note: doing this BU since it can then fail, but if we did it AU would it be
+-- out of sync? No, I thought not, but if someone knows the correct answer, or
+-- indeed whether this even makes a difference, please let me know.
+CREATE OR REPLACE FUNCTION blog.trackback_bu() RETURNS trigger as '
+    BEGIN
+        IF OLD.entry_id = NEW.entry_id THEN
+            IF OLD.status <> ''acc'' AND NEW.status = ''acc'' THEN
+                UPDATE blog.entry SET trackbacks = trackbacks + 1 WHERE id = OLD.entry_id;
+            END IF;
+            IF OLD.status = ''acc'' AND NEW.status <> ''acc'' THEN
+                UPDATE blog.entry SET trackbacks = trackbacks - 1 WHERE id = OLD.entry_id;
+            END IF;
+        ELSE
+            IF OLD.status = ''acc'' THEN
+                UPDATE blog.entry SET trackbacks = trackbacks - 1 WHERE id = OLD.entry_id;
+            END IF;
+            IF NEW.status = ''acc'' THEN
+                UPDATE blog.entry SET trackbacks = trackbacks + 1 WHERE id = NEW.entry_id;
+            END IF;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER trackback_bu BEFORE UPDATE ON blog.trackback
+    FOR EACH ROW EXECUTE PROCEDURE blog.trackback_bu();
+
+-- function: trackback_bd
+CREATE FUNCTION blog.trackback_bd() RETURNS trigger as '
+    BEGIN
+        IF OLD.status = ''acc'' THEN
+            UPDATE blog.entry SET trackbacks = trackbacks - 1 WHERE id = OLD.entry_id;
+        END IF;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+CREATE TRIGGER trackback_bd BEFORE DELETE ON blog.trackback
+    FOR EACH ROW EXECUTE PROCEDURE blog.trackback_bd();
 
 -- ----------------------------------------------------------------------------

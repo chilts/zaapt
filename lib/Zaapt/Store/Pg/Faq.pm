af0444aead506857c45b7098a8a0f4a937bd4142
Andrew Chilton <andychilton@gmail.com>
2007-10-08 11:19:23 +0000

    - update the Menu Model to use the new Pg library methods

--- lib/Zaapt/Store/Pg/Faq.pm
+++ lib/Zaapt/Store/Pg/Faq.pm
@@ -5,102 +5,66 @@ use base qw( Zaapt::Store::Pg Zaapt::Model::Faq );
 use strict;
 use warnings;
 
+use Zaapt::Store::Pg::Common;
+
 ## ----------------------------------------------------------------------------
 # constants
 
-# table names
-my $faq_tablename = "faq.faq f";
-my $question_tablename = "faq.question q";
-my $type_tablename = "common.type t";
+# schema name
+my $schema = 'faq';
 
-# helper
-my $faq_cols = __PACKAGE__->_mk_cols( 'f', qw(id name title description r:admin_id r:view_id r:edit_id) );
-my $question_cols = __PACKAGE__->_mk_cols( 'q', qw(id faq_id type_id question answer display) );
-my $type_cols = __PACKAGE__->_mk_cols( 't', qw(id name) );
+my $tables = {
+    faq => {
+        schema => $schema,
+        name => 'faq',
+        prefix => 'f',
+        cols => [ qw(id name title description r:admin_id r:view_id r:edit_id) ],
+    },
+    question => {
+        schema => $schema,
+        name => 'question',
+        prefix => 'q',
+        cols => [
+            'id',
+            [ 'faq_id', 'fk', 'f_id' ],
+            [ 'type_id', 'fk', 't_id' ],
+            qw(question answer display)
+        ],
+    },
+    type => Zaapt::Store::Pg::Common->_get_table( 'type' ),
+};
 
-# joins
-my $f_q_join = "JOIN $question_tablename ON (f.id = q.faq_id)";
-my $q_t_join = "JOIN $type_tablename ON (q.type_id = t.id)";
+my $join = {
+    f_q => "JOIN $schema.question q ON (f.id = q.faq_id)",
+    q_t => "JOIN $tables->{type}{schema}.$tables->{type}{name} $tables->{type}{prefix} ON (q.type_id = $tables->{type}{prefix}.id)",
+};
+
+## ----------------------------------------------------------------------------
+
+# creates {sql_fqt} and {sql_sel_cols}
+__PACKAGE__->_mk_sql( $schema, $tables );
+
+# generate the Perl method accessors
+__PACKAGE__->_mk_db_accessors( $schema, $tables );
+
+## ----------------------------------------------------------------------------
+# simple accessors
+
+# create some reusable sql
+my $main_cols = "$tables->{faq}{sql_sel_cols}, $tables->{question}{sql_sel_cols}, $tables->{type}{sql_sel_cols}";
+my $main_tables = "$tables->{faq}{sql_fqt} $join->{f_q} $join->{q_t}";
 
 # faq
-my $ins_faq = __PACKAGE__->_mk_ins( 'faq.faq', 'name', 'title', 'description', 'admin_id', 'view_id', 'edit_id' );
-my $upd_faq = __PACKAGE__->_mk_upd( 'faq.faq', 'id', qw(name title description admin_id view_id edit_id) );
-my $del_faq = __PACKAGE__->_mk_del('faq.faq', 'id');
-my $sel_faq = "SELECT $faq_cols FROM $faq_tablename WHERE f.id = ?";
-my $sel_faq_all = "SELECT $faq_cols FROM $faq_tablename ORDER BY f.name";
-my $sel_faq_using_name = "SELECT $faq_cols FROM $faq_tablename WHERE f.name = ?";
+__PACKAGE__->_mk_selecter( $schema, $tables->{faq} );
+__PACKAGE__->_mk_selecter_using( $schema, $tables->{faq}, 'name' );
+__PACKAGE__->mk_select_rows( 'sel_question_all', "SELECT $tables->{faq}{sql_sel_cols} FROM $tables->{faq}{sql_fqt} ORDER BY f.name" );
 
 # question
-my $ins_question = __PACKAGE__->_mk_ins( 'faq.question', 'faq_id', 'type_id', 'question', 'answer' );
-my $upd_question = __PACKAGE__->_mk_upd( 'faq.question', 'id', qw(faq_id type_id question answer display) );
-my $del_question = __PACKAGE__->_mk_del( 'faq.question', 'id');
-my $sel_question = "SELECT $faq_cols, $question_cols, $type_cols FROM $faq_tablename $f_q_join $q_t_join WHERE q.id = ?";
-my $sel_all_questions_in = "SELECT $faq_cols, $question_cols, $type_cols FROM $faq_tablename $f_q_join $q_t_join WHERE f.id = ? ORDER BY q.display";
-
-# type
-my $sel_all_types = "SELECT $type_cols FROM $type_tablename ORDER BY name";
+__PACKAGE__->mk_select_row( 'sel_question', "SELECT $main_cols FROM $main_tables WHERE q.id = ?", [ 'q_id' ] );
+__PACKAGE__->mk_select_rows( 'sel_question_all_in', "SELECT $main_cols FROM $main_tables WHERE c.id = ? ORDER BY p.name", [ 'c_id' ] );
 
 ## ----------------------------------------------------------------------------
 # methods
-
-# table: Faq
-
-sub ins_faq {
-    my ($self, $hr) = @_;
-    $self->_do( $ins_faq, $hr->{f_name}, $hr->{f_title}, $hr->{f_description}, $hr->{_admin}, $hr->{_view}, $hr->{_edit} );
-}
-
-sub upd_faq {
-    my ($self, $hr) = @_;
-    $self->_do( $upd_faq, $hr->{f_name}, $hr->{f_title}, $hr->{f_description}, $hr->{_admin}, $hr->{_view}, $hr->{_edit}, $hr->{f_id} );
-}
-
-sub del_faq {
-    my ($self, $hr) = @_;
-    $self->_do( $del_faq, $hr->{f_id} );
-}
-
-sub sel_faq_all {
-    my ($self) = @_;
-    return $self->_rows( $sel_faq_all );
-}
-
-sub sel_faq {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_faq, $hr->{f_id} );
-}
-
-sub sel_faq_using_name {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_faq_using_name, $hr->{f_name} );
-}
-
-# table: Content
-
-sub ins_question {
-    my ($self, $hr) = @_;
-    $self->_do( $ins_question, $hr->{f_id}, $hr->{t_id}, $hr->{q_question}, $hr->{q_answer} );
-}
-
-sub upd_question {
-    my ($self, $hr) = @_;
-    $self->_do( $upd_question, $hr->{f_id}, $hr->{t_id}, $hr->{q_question}, $hr->{q_answer}, $hr->{q_display}, $hr->{q_id} );
-}
-
-sub del_question {
-    my ($self, $hr) = @_;
-    $self->_do( $del_question, $hr->{q_id} );
-}
-
-sub sel_question {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_question, $hr->{q_id} );
-}
-
-sub sel_all_questions_in {
-    my ($self, $hr) = @_;
-    return $self->_rows( $sel_all_questions_in, $hr->{f_id} );
-}
 
 sub _nuke {
     my ($self) = @_;
--- lib/Zaapt/Store/Pg/Menu.pm
+++ lib/Zaapt/Store/Pg/Menu.pm
@@ -8,97 +8,76 @@ use warnings;
 ## ----------------------------------------------------------------------------
 # constants
 
-# table names
-my $menu_tablename = "menu.menu m";
-my $item_tablename = "menu.item i";
+# schema name
+my $schema = 'menu';
 
-# helper
-my $menu_cols = __PACKAGE__->_mk_cols( 'm', qw(id name title description r:admin_id r:view_id r:edit_id) );
-my $item_cols = __PACKAGE__->_mk_cols( 'i', qw(id menu_id display level url text ishtml) );
+my $tables = {
+    menu => {
+        schema => $schema,
+        name => 'menu',
+        prefix => 'm',
+        cols => [ qw(id name title description r:admin_id r:view_id r:edit_id) ],
+    },
+    item => {
+        schema => $schema,
+        name => 'item',
+        prefix => 'i',
+        cols => [
+            'id',
+            [ 'menu_id', 'fk', 'm_id' ],
+            qw(display level url text ishtml)
+        ],
+    },
+};
+
+my $join = {
+    m_i => "JOIN $schema.item i ON (m.id = i.menu_id)",
+};
 
 my $item_seq = 'menu.item_id_seq';
 
-# joins
-my $m_i_join = "JOIN $item_tablename ON (m.id = i.menu_id)";
+## ----------------------------------------------------------------------------
 
-# menu
-my $ins_menu = __PACKAGE__->_mk_ins( 'menu.menu', 'name', 'title', 'description', 'admin_id', 'view_id', 'edit_id' );
-my $upd_menu = __PACKAGE__->_mk_upd( 'menu.menu', 'id', qw(name title description admin_id view_id edit_id) );
-my $del_menu = __PACKAGE__->_mk_del('menu.menu', 'id');
-my $sel_menu = "SELECT $menu_cols FROM $menu_tablename WHERE m.id = ?";
-my $sel_menu_all = "SELECT $menu_cols FROM $menu_tablename ORDER BY m.name";
-my $sel_menu_using_name = "SELECT $menu_cols FROM $menu_tablename WHERE m.name = ?";
+# creates {sql_fqt} and {sql_sel_cols}
+__PACKAGE__->_mk_sql( $schema, $tables );
 
-# item
-my $ins_item = __PACKAGE__->_mk_ins( 'menu.item', 'menu_id', 'level ', 'url', 'text', 'ishtml' );
-my $upd_item = __PACKAGE__->_mk_upd( 'menu.item', 'id', qw(menu_id display level url text ishtml) );
-my $del_item = __PACKAGE__->_mk_del( 'menu.item', 'id');
-my $sel_item = "SELECT $menu_cols, $item_cols FROM $menu_tablename $m_i_join WHERE i.id = ?";
-my $sel_all_items_in = "SELECT $menu_cols, $item_cols FROM $menu_tablename $m_i_join WHERE m.id = ? ORDER BY i.display";
-
-# other
-my $sel_items_for_move_up = "SELECT $item_cols FROM $item_tablename WHERE i.menu_id = ? AND i.display <= ? ORDER BY i.display DESC LIMIT 2";
-my $sel_items_for_move_dn = "SELECT $item_cols FROM $item_tablename WHERE i.menu_id = ? AND i.display >= ? ORDER BY i.display LIMIT 2";
+# generate the Perl method accessors
+__PACKAGE__->_mk_db_accessors( $schema, $tables );
 
 ## ----------------------------------------------------------------------------
-# methods
+# simple accessors
 
-# table: Menu
+# create some reusable sql
+my $main_cols = "$tables->{menu}{sql_sel_cols}, $tables->{item}{sql_sel_cols}";
+my $main_tables = "$tables->{menu}{sql_fqt} $join->{m_i}";
 
-sub ins_menu {
-    my ($self, $hr) = @_;
-    $self->_do( $ins_menu, $hr->{m_name}, $hr->{m_title}, $hr->{m_description}, $hr->{_admin}, $hr->{_view}, $hr->{_edit} );
-}
+# menu
+__PACKAGE__->_mk_selecter( $schema, $tables->{menu} );
+__PACKAGE__->_mk_selecter_using( $schema, $tables->{menu}, 'name' );
+__PACKAGE__->mk_select_rows( 'sel_menu_all', "SELECT $tables->{menu}{sql_sel_cols} FROM $tables->{menu}{sql_fqt} ORDER BY m.name", [] );
 
-sub upd_menu {
-    my ($self, $hr) = @_;
-    $self->_do( $upd_menu, $hr->{m_name}, $hr->{m_title}, $hr->{m_description}, $hr->{_admin}, $hr->{_view}, $hr->{_edit}, $hr->{m_id} );
-}
+# item
+__PACKAGE__->mk_select_row( 'sel_item', "SELECT $main_cols FROM $main_tables WHERE i.id = ?", [ 'i_id' ] );
+__PACKAGE__->mk_select_rows( 'sel_item_all_in', "SELECT $main_cols FROM $main_tables WHERE m.id = ? ORDER BY i.display", [ 'm_id' ] );
 
-sub del_menu {
-    my ($self, $hr) = @_;
-    $self->_do( $del_menu, $hr->{m_id} );
-}
+## ----------------------------------------------------------------------------
+# complex accessors
 
-sub sel_menu_all {
-    my ($self) = @_;
-    return $self->_rows( $sel_menu_all );
-}
-
-sub sel_menu {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_menu, $hr->{m_id} );
-}
-
-sub sel_menu_using_name {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_menu_using_name, $hr->{m_name} );
-}
-
-# table: Item
-
+# need this since i_display should be left to it's default value
+# ToDo: put an extra thing in Pg.pm to help do this (e.g. def:display)
+my $ins_item = __PACKAGE__->_mk_ins( 'menu.item', 'menu_id', 'level ', 'url', 'text', 'ishtml' );
 sub ins_item {
     my ($self, $hr) = @_;
     $self->_do( $ins_item, $hr->{m_id}, $hr->{i_level}, $hr->{i_url}, $hr->{i_text}, $hr->{i_ishtml} );
 }
 
-sub upd_item {
-    my ($self, $hr) = @_;
-    $self->_do( $upd_item, $hr->{m_id}, $hr->{i_display}, $hr->{i_level}, $hr->{i_url}, $hr->{i_text}, $hr->{i_ishtml}, $hr->{i_id} );
-}
-
-sub del_item {
-    my ($self, $hr) = @_;
-    $self->_do( $del_item, $hr->{i_id} );
-}
-
-sub sel_item {
-    my ($self, $hr) = @_;
-    return $self->_row( $sel_item, $hr->{i_id} );
-}
+# other
+my $i_t = $tables->{item};
+my $sel_items_for_move_up = "SELECT $i_t->{sql_sel_cols} FROM $i_t->{sql_fqt} WHERE i.menu_id = ? AND i.display <= ? ORDER BY i.display DESC LIMIT 2";
+my $sel_items_for_move_dn = "SELECT $i_t->{sql_sel_cols} FROM $i_t->{sql_fqt} WHERE i.menu_id = ? AND i.display >= ? ORDER BY i.display LIMIT 2";
 
 sub upd_item_move {
-    my ($self, $hr, $sql) = @_;
+    my ($self, $sql, $hr) = @_;
 
     # start a transaction
     $self->dbh()->begin_work();
@@ -126,18 +105,13 @@ sub upd_item_move {
 sub upd_item_move_up {
     my ($self, $hr) = @_;
 
-    $self->upd_item_move( $hr, $sel_items_for_move_up );
+    $self->upd_item_move( $sel_items_for_move_up, $hr );
 }
 
 sub upd_item_move_dn {
     my ($self, $hr) = @_;
 
-    $self->upd_item_move( $hr, $sel_items_for_move_dn );
-}
-
-sub sel_all_items_in {
-    my ($self, $hr) = @_;
-    return $self->_rows( $sel_all_items_in, $hr->{m_id} );
+    $self->upd_item_move( $sel_items_for_move_dn, $hr );
 }
 
 sub _nuke {
